09107823afecbe1bcc7e1a3ff78b70a9ab29b3db
Nicholas3388 <tulip-wangwei@163.com>
2017-04-07 14:22:50 +0800

    Add sources

---
+++ README.md
@@ -0,0 +1,6 @@
+## nRF51822 sample
+
+This is a sample to show how to create connection between ESP32 and 
+nRF51822. In this sample, when ESP32 write to string "on"/"off" to 
+the characteristic "6e400002-b5a3-f393-e0a9-e50e24dcca9e", which proviced 
+by nRF51, the nRF51 device will turn on/off the LED on board.
---
+++ examples/ble_peripheral/ble_app_uart/pca10028/s110/arm4/_build/app_fifo.d
@@ -0,0 +1,8 @@
+.\_build\app_fifo.o: ..\..\..\..\..\..\components\libraries\fifo\app_fifo.c
+.\_build\app_fifo.o: ..\..\..\..\..\..\components\libraries\fifo\app_fifo.h
+.\_build\app_fifo.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\_build\app_fifo.o: C:\Keil\ARM\ARMCC\bin\..\include\stdlib.h
+.\_build\app_fifo.o: ..\..\..\..\..\..\components\softdevice\s110\headers\nrf_error.h
+.\_build\app_fifo.o: ..\..\..\..\..\..\components\libraries\util\app_util.h
+.\_build\app_fifo.o: C:\Keil\ARM\ARMCC\bin\..\include\stdbool.h
+.\_build\app_fifo.o: ..\..\..\..\..\..\components\device\compiler_abstraction.h
